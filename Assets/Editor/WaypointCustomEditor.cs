061473b838f16f1305058fca039a806aa739971f
LSO107 <34274877+LSO107@users.noreply.github.com>
2019-11-17 22:12:39 +0000

    Guard State Machine
    Guard States
    Waypoints for patrolling defined
    Guard behaviour needs implementing

---
+++ Assets/Editor/WaypointCustomEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(Waypoint))]
+    public class WaypointCustomEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            var myTarget = (Waypoint) target;
+
+            if (GUILayout.Button("Calculate Linked Nodes"))
+            {
+                myTarget.CalculateNLinkedNodes();
+            }
+        }
+    }
+}
