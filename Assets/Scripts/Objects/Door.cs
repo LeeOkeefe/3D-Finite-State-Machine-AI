19c4b0bc6cd38c88250674eec6578f3a72e630c3
LSO107 <34274877+LSO107@users.noreply.github.com>
2019-11-28 20:12:48 +0000

    Interactive Objects
    Level design
    Interactive Objects (open doors & drawers)
    Walkable Stairs (Ramp colliders)

---
+++ Assets/Scripts/Objects/Door.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Objects
+{
+    internal class Door : MonoBehaviour, IInteractive
+    {
+        [SerializeField] private Vector3 angle;
+
+        public void Interact()
+        {
+            StartCoroutine(OpenDoor());
+        }
+
+        private IEnumerator OpenDoor()
+        {
+            var endRotation = transform.rotation * Quaternion.Euler(angle);
+
+            while (transform.rotation != endRotation)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, endRotation, Time.deltaTime * 1f);
+                yield return null;
+            }
+
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/Drawer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Objects
+{
+    internal sealed class Drawer : MonoBehaviour, IInteractive
+    {
+        [SerializeField] private Vector3 position;
+        [SerializeField] private float lerpTime = 1f;
+
+        private float m_CurrentLerpTime;
+
+        public void Interact()
+        {
+            StartCoroutine(OpenDrawer());
+        }
+
+        private IEnumerator OpenDrawer()
+        {
+            var endPosition = transform.position + position;
+
+            while (m_CurrentLerpTime < lerpTime)
+            {
+                m_CurrentLerpTime += Time.deltaTime;
+                transform.position = Vector3.Lerp(transform.position, endPosition, Time.deltaTime * lerpTime);
+                yield return null;
+            }
+
+            Destroy(this);
+        }
+    }
+}
