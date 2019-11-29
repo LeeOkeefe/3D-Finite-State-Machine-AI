c760e4cbd15e043d679d785516a694f5220e7241
LSO107 <34274877+LSO107@users.noreply.github.com>
2019-11-29 00:13:57 +0000

    ForceObject
    Knock objects over

---
+++ Assets/Scripts/Objects/ForceObject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Objects
+{
+    internal sealed class ForceObject : MonoBehaviour
+    {
+        [SerializeField]
+        [Range(100, 500)]
+        private float force = 150;
+
+        private Rigidbody Rb => GetComponent<Rigidbody>();
+
+        /// <summary>
+        /// Calculates the opposite direction of the given collider,
+        /// and adds force to this RigidBody in that direction
+        /// </summary>
+        private void AddForce(Collider col, Rigidbody rb)
+        {
+            var direction = (transform.position - col.gameObject.transform.position).normalized;
+
+            rb.AddForce(direction * force);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            AddForce(other.GetComponent<Collider>(), Rb);
+        }
+    }
+}
