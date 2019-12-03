2309bebb9595f7afe6235c8445a85aee29aee92a
LSO107 <34274877+LSO107@users.noreply.github.com>
2019-12-03 17:12:45 +0000

    Main Menu Base Set up
    Created Base for main menu
    Basic functionality but needs some more options working.

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Menu
+{
+    internal sealed class Menu : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup m_MainMenu;
+        [SerializeField] private CanvasGroup m_Options;
+
+        public void StartGame()
+        {
+            //TODO: CHANGE NAME ("MAP" is currently main scene)
+            SceneManager.LoadScene("Map");
+        }
+
+        public void OpenOptions()
+        {
+            UserInterfaceUtils.ToggleCanvasGroup(m_MainMenu, false);
+            UserInterfaceUtils.ToggleCanvasGroup(m_Options, true);
+        }
+
+        public void CloseOptions()
+        {
+            UserInterfaceUtils.ToggleCanvasGroup(m_MainMenu, true);
+            UserInterfaceUtils.ToggleCanvasGroup(m_Options, false);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
