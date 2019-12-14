f5b950f365b1418d46acdacd2a76cfde27f0f48b
LSO107 <34274877+LSO107@users.noreply.github.com>
2019-12-14 17:17:43 +0000

    console writer

---
+++ Assets/Scripts/Guard/GuardIdle.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Guard
+{
+    public class GuardIdle : MonoBehaviour, IBehaviour
+    {
+        private GuardBehaviour m_Behaviour;
+        private GuardState m_CurrentState;
+
+        private float m_Timer;
+        private float m_MaxTime;
+
+        private System.Random m_Random;
+
+        private void Update()
+        {
+            if (m_CurrentState != GuardState.Idle)
+                return;
+
+            m_Timer += Time.deltaTime;
+
+            if (m_Timer >= m_MaxTime)
+            {
+                m_Behaviour.EndIdle();
+            }
+        }
+
+        public void Initialize()
+        {
+            m_Behaviour = GetComponent<GuardBehaviour>();
+            m_Random = new System.Random();
+        }
+
+        public void UpdateState(GuardState newState)
+        {
+            m_Timer = 0;
+            m_MaxTime = m_Random.Next(2, 5);
+            m_CurrentState = newState;
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/MainDoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Items;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Objects
+{
+    internal class MainDoor : Door
+    {
+        [SerializeField] private Inventory inventory;
+        [SerializeField] private int[] itemIdRequired;
+
+        public override void Interact()
+        {
+            if (!itemIdRequired.All(i => inventory.HasItem(i)))
+            {
+                StartCoroutine(GameManager.Instance.ShowMessage("Get the documents before leaving!"));
+            }
+            else
+            {
+                SceneManager.LoadScene("EndLevel");
+            }
+        }
+    }
+}
